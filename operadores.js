2603044f95506017275bccfda88c455a1e06aadc
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-20 22:40:11 -0600

    Uso de operadores

---
+++ operadores.js
@@ -0,0 +1,27 @@
+var asignacion = 2
+console.log(asignacion)
+
+
+//Operadores aritmeticos
+var suma = asignacion + 2
+console.log(suma)
+
+var resta = asignacion - 1
+console.log(resta)
+
+var multiplicacion = 4 *  5
+multiplicacion *= multiplicacion
+console.log(multiplicacion)
+
+
+//Operadores logicos
+var comparacion = false
+if(resta != suma)
+{
+    comparacion = true
+}
+console.log(comparacion)
+
+//Ternarios
+var ternario = resta < suma ? console.log("La resta es menor a la suma") : console.log("La resta es mayor a la suma")
+
