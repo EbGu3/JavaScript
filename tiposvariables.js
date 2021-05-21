6cff8d12316f2caeb468d5e6d17a325dc6b0f448
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-20 22:18:28 -0600

    Tipos de variables en javascritp

---
+++ tiposvariables.js
@@ -0,0 +1,30 @@
+//Variable de tipo string
+var Nombre = "Alejo"
+console.log(Nombre)
+
+//Variable de tipo numero
+var miNumero = 50
+console.log(miNumero)
+
+//Variable de tipo decima
+var Pi = 3.1416
+console.log(Pi)
+
+//Variables de tipo booleano
+var Verdadero = true
+var Falso = false
+console.log(Verdadero)
+console.log(Falso)
+
+//Variable de tipo nulo
+var Nulo = null
+console.log(Nulo)
+
+//Variable de tipo indefinido
+var Indefinido = undefined
+console.log(Indefinido)
+
+//Variable de tipo constante
+const _pi = Math.PI
+console.log(_pi)
+
