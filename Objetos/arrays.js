8d955154f46c6f1879d48f11c8fca0a821a3a622
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-21 10:30:53 -0600

    Uso de arreglos

---
+++ Objetos/arrays.js
@@ -0,0 +1,33 @@
+var permitido = "Usuario Permitido"
+var miFuncion1 = acceso => acceso
+var persona = {
+    Nombre : "Pepe", 
+    MiAutoMovil : [
+        miLlanta = {
+            Marca : "Micheline"
+        },
+        miPintura = {
+            Marca : "Marca",
+            Color: "Rojo"            
+        }
+    ]
+}
+
+var MiArray = [
+    "Jorge", 
+    "Maria", 
+    "David",  
+    true, 
+    3, 
+    8.9, 
+    [
+        "OtroArray", 
+        "2020"
+    ],
+    miFuncion1(permitido),
+    persona
+]
+
+
+
+console.log(MiArray[8].MiAutoMovil[1].Color)
