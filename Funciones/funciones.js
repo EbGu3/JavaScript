d670367c851f6a24365fb926485b02ed237ce2ee
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-21 08:48:49 -0600

    Uso de funciones con =>

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -15,3 +15,31 @@ var _resta = function(a, b) {
 saludo()
 console.log("Mi suma es: " + miSuma(8,9))
 console.log("La resta es: " + _resta(8,1))
+
+
+//Funciones de flecha
+var acceso = true
+
+ //Funcion que le pasamos parametros
+var AccesoU = acceso => acceso
+
+//Funcion que no le pasamos parametros
+var AccesoU2 = () => false
+    AccesoU2(acceso) == true ? console.log("Usuario permitido") : console.log("Usuario no permitido")
+
+//Funcion con mas de un parametro
+var AccesoU3 = (nombre, acceso) => console.log(`El usuario ${nombre} tiene acceso? ${acceso}`)
+    AccesoU3("Juan", true)
+
+
+//Funcion para retornar mas de dos instrucciones
+var Acceso4 = () => {
+    console.log("Usuario en ejecucion")
+    return true;
+}
+
+    Acceso4()
+
+
+
+
