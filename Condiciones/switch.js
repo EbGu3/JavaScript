78b7eb11ee8082e75098748e9e0ec27dbab2dc19
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-21 10:49:46 -0600

    Uso de switch

---
+++ Condiciones/switch.js
@@ -0,0 +1,17 @@
+//Recibe una expresion y de acuerdo a ella, ejecuta un caso
+
+var OpcionSeleccionado = "3"
+
+switch(OpcionSeleccionado) {
+    case 1:
+        console.log("Pidio pizza")
+        break;
+
+    case 2:
+        console.log("Pidio hamburguesa")
+        break;
+
+    default:
+        console.log("Pidio cafe")
+        break;
+}
