6c7f0abe65f8058f0314200957c87a6c1721be47
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-20 22:27:19 -0600

    Uso de metodos de un string

---
+++ string.js
@@ -0,0 +1,14 @@
+var _Nombre = 'Alejo'
+var _Edad = 25
+
+console.log(`¡Hola mi nombre es ${_Nombre}! y mi edad es de ${_Edad}`)
+
+//Texto en mayuscula
+var NombreMayuscula = _Nombre.toUpperCase()
+console.log(NombreMayuscula)
+
+//Texto en minustcula
+console.log(_Nombre.toLowerCase())
+
+//Longitud de mi cadena
+console.log(_Nombre.length)
