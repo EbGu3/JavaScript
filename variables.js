ae7c674ed32778f06d2aef5a9858ce7836159e28
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-05-20 22:11:56 -0600

    Uso de variables de tipo var, let y const

---
+++ variables.js
@@ -0,0 +1,19 @@
+//Declara variables globales, var
+var saludo = "Hola mundo"
+if(saludo) {
+    var saludo = "hola"
+    console.log(saludo)
+}
+console.log(saludo)
+
+//let, variables limitadas al bloque donde se define
+let nombre = "Josue"
+if(nombre) {
+    let nombre = "Daniel"
+    console.log(nombre)
+}
+console.log(nombre)
+
+//const, variables que no pueden cambiar
+const pi = 3.1416
+console.log(pi)
